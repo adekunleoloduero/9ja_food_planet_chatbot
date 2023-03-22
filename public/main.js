b37f707f356b1ceec37bc25f30f2c57ef38b5cf8
Sheriff Adekunle Tajudeen <dnexteinstein2@gmail.com>
2023-03-22 11:02:58 +0100

    style chat interface

--- public/main.js
+++ public/main.js
@@ -1,8 +1,11 @@
 const chatMessages = document.getElementById('chat-messages');
 const sendButton = document.getElementById('send-button');
 const messageBox = document.getElementById('message-box');
+const chatInterface = document.getElementById('chat-interface');
+
 var menuType = 'mainMenu';
 const botName = `<p class="bot-name">9ja FP Chatbot</p>`;
+const customers = {};
 const cart = {};
 
 
@@ -21,10 +24,9 @@ const mealMenu = {
     "101": {desc: 'Jollof rice with chicken and salad', price: 1900},
     "102": {desc: 'Offada rice and beef', price: 1550},
     "103": {desc: 'Beans, plantain and sauce', price: 1100},
-    "104":  {desc: 'Moin-moin', price: 600}
+    "104":  {desc: 'Moin-moin', price: 600},
+    "105": {desc: 'Yam porridge and fish', price: 900},
 }
-
-const customers = {};
 
 
 //Socket connection
@@ -35,18 +37,18 @@ const socket = io();
 
 const createMessage = (msgText, msgClass) => {
     const message = document.createElement('div');
-    message.classList.add(msgClass);
+    message.classList.add(msgClass, "messages");
     message.innerHTML = msgText;
     return message;
 }
 
 
 const showMainMenu = () => {
-    const date = Date.now()
+    const date = new Date();
     const msg = `
     ${botName}
-    <p>${date}</p>
-    <p>Hi!</p>
+    <p class="time">${date.getHours()}:${date.getMinutes()}</p><br>
+    <p>Hi,</p><br>
     <h4>Select an option to proceed:</h4>
     <ul>
         <li>1. ${mainMenu["1"]}</li>
@@ -63,7 +65,7 @@ const showMainMenu = () => {
 function printMessages(customerMsgContent, botMsgContent) {
     if (customerMsgContent) {
         //Customer message
-        const customerMsg = createMessage(customerMsgContent, "customer-msg", "You");
+        const customerMsg = createMessage(customerMsgContent, "customer-msg");
         chatMessages.append(customerMsg);
     }
 
@@ -73,6 +75,11 @@ function printMessages(customerMsgContent, botMsgContent) {
     
     messageBox.value = "";
     messageBox.focus();
+}
+
+
+function scrollToLastMessage() {
+    window.scrollTo(0, document.body.scrollHeight);
 }
 
 
@@ -87,16 +94,17 @@ socket.on("showMainMenu", (data) => {
 
     let customerMsgContent;
     if (data.msg) {
-        date1 = Date.now();
+        date1 = new Date();
         customerMsgContent = `
         <p>You</p>
-        <p>${date1}</p>
+        <p class="time">${date1.getHours()}:${date1.getMinutes()}</p><br>
         <p class="customer-reply">${data.msg}</p>
         `
     }
 
     const mainMenu = showMainMenu();
     printMessages(customerMsgContent, mainMenu);
+    scrollToLastMessage();
 });
 
 //Create an Order
@@ -105,22 +113,22 @@ socket.on("createOrder", (data) => {
     let date1, date2;
 
     //Cutomer Message
-    date1 = Date.now();
+    date1 = new Date();
     const customerMsgContent = `
     <p>You</p>
-    <p>${date1}</p>
+    <p class="time">${date1.getHours()}:${date1.getMinutes()}</p><br>
     <p class="customer-reply">${data.msg}</p>
     `
 
     //Chatbot message
-    date2 = Date.now();
+    date2 = new Date();
     const botMsgContent = 
     `
     ${botName}
-    <p>${date2}</p>
+    <p class="time">${date2.getHours()}:${date2.getMinutes()}</p><br>
     <h4>Place an order:</h4>
     <ul>
-        <li>100. ${mealMenu["100"].desc} - ${mealMenu["100"].price}</li>
+        <li>100. ${mealMenu["100"].desc}: <i class='fa-solid fa-naira-sign'></i>${mealMenu["100"].price}</li>
         <li>101. ${mealMenu["101"].desc} - ${mealMenu["101"].price}</li>
         <li>102. ${mealMenu["102"].desc} - ${mealMenu["102"].price}</li>
         <li>103. ${mealMenu["103"].desc} - ${mealMenu["103"].price}</li>
@@ -129,75 +137,94 @@ socket.on("createOrder", (data) => {
     </ul>
     `
     printMessages(customerMsgContent, botMsgContent);
+    scrollToLastMessage();
 });
 
 //Add order to cart
 socket.on("addTocart", (data) => {
     menuType = data.menuType;
-    cart[data.id] = mealMenu[data.msg];
+    cart[data.id] = data.msg;
     let date1, date2;
 
     //Cutomer Message
-    date1 = Date.now();
+    date1 = new Date();
     const customerMsgContent = `
     <p>You</p>
-    <p>${date1}</p>
+    <p class="time">${date1.getHours()}:${date1.getMinutes()}</p><br>
     <p class="customer-reply">${data.msg}</p>
     `
 
     //Chatbot message
-    date2 = Date.now();
+    date2 = new Date()
     const botMsgContent = 
     `
     ${botName}
-    <p>${date2}</p>
-    <p>Order has been added to cart</p>
+    <p class="time">${date2.getHours()}:${date2.getMinutes()}</p><br>
+    <p>Order has been added to cart</p><br>
+    <br>
     <ul class="options">
         <li>99. Checkout order</li>
         <li>00. Back to Main Menu</li>
     </ul>
     `
-    printMessages(customerMsgContent, botMsgContent)   
+    printMessages(customerMsgContent, botMsgContent)   ;
+    scrollToLastMessage();
 });
 
 
 //Checkout order
 socket.on("orderPlaced", (data) => {
-    menuType = data.menuType
-    let order;
+    menuType = data.menuType;
+    let customerMsgContent;
+    let botMsgContent;
+    let orderID;
     if (cart.hasOwnProperty(data.id)) {
-        order = cart[data.id];
+        orderID = cart[data.id];
         delete cart[data.id];
     }
-    
-    console.log(data);
 
-    customers[data.id].push(order); //Save order;
+    customers[data.id].push(mealMenu[orderID]); //Save order;
     let date1, date2;
 
     //Cutomer Message
-    date1 = Date.now();
-    const customerMsgContent = `
+    date1 = new Date();
+    customerMsgContent = `
     <p>You</p>
-    <p>${date1}</p>
+    <p class="time">${date1.getHours()}:${date1.getMinutes()}</p><br>
     <p class="customer-reply">${data.msg}</p>
     `
 
-    //Chatbot message
-    date2 = Date.now();
-    const botMsgContent = 
-    `
-    ${botName}
-    <p>${date2}</p>
-    <p>Your order is being processed...</p>
-    <h4>Order Details:</h4>
-    <p>Description: ${order.desc}</p>
-    <p>Price: ${order.price}</p>
-    <ul class="options">
-        <li>00. Back to Main Menu</li>
-    </ul>
-    `
-    printMessages(customerMsgContent, botMsgContent)   
+    if (!orderID) {
+        date2 = new Date();
+        botMsgContent = 
+        `
+        ${botName}
+        <p class="time">${date2.getHours()}:${date2.getMinutes()}</p><br>
+        <p>Your cart is empty</p><br>
+        <ul class="options">
+            <li>00. Back to Main Menu</li>
+        </ul>
+        `
+    } else {
+
+        //Chatbot message
+        date2 = new Date();
+        botMsgContent = 
+        `
+        ${botName}
+        <p class="time">${date2.getHours()}:${date2.getMinutes()}</p><br>
+        <p>Your order is being processed...</p><br>
+        <h4>Order Details:</h4>
+        <p>Description: ${mealMenu[orderID].desc}</p>
+        <p>Price: ${mealMenu[orderID].price}</p><br>
+        <br>
+        <ul class="options">
+            <li>00. Back to Main Menu</li>
+        </ul>
+        `
+    }
+    printMessages(customerMsgContent, botMsgContent);
+    scrollToLastMessage();
 })
 
 //See orders history
@@ -205,13 +232,15 @@ socket.on("seeOrdersHistory", (data) => {
     menuType = data.menuType;
     let orderHistoryText;
     const orders = customers[data.id];
-    if (orders.length < 1) {
-        orderHistoryText = `<p>Your order history is currently empty</p>`
+    if (orders == undefined || orders.length < 1) {
+        orderHistoryText = `
+        <p>Your order history is currently empty.</p><br>
+        `
     } else {
         let allOrders = '';
 
         for (const order of orders) {
-            let orderText = `<p>${order.desc}: ${order.price}</p>`
+            let orderText = `<p>- ${order.desc}: ${order.price}</p>`
             allOrders += orderText;
         }
         orderHistoryText = allOrders;
@@ -220,27 +249,28 @@ socket.on("seeOrdersHistory", (data) => {
     let date1, date2;
 
     //Cutomer Message
-    date1 = Date.now();
+    date1 = new Date();
     const customerMsgContent = `
     <p class="customer-name">You</p>
-    <p class="time">${date1}</p>
+    <p class="time">${date1.getHours()}:${date1.getMinutes()}</p><br>
     <p class="customer-reply">${data.msg}</p>
     `
    
     //Chatbot message
-    date2 = Date.now();
+    date2 = new Date();
     const botMsgContent = 
     `
     ${botName}
-    <p class="time">${date2}</p>
+    <p class="time">${date2.getHours()}:${date2.getMinutes()}</p><br>
     <h4>Orders history:</h4>
     ${orderHistoryText}
-    <hr>
+    <br>
     <ul class="options">
         <li>00. Back to Main Menu</li>
     </ul>
     `
-    printMessages(customerMsgContent, botMsgContent)   
+    printMessages(customerMsgContent, botMsgContent);
+    scrollToLastMessage();
 })
 
 
@@ -255,33 +285,34 @@ socket.on("seeCurrentOrder", (data) => {
         currentOrderText = `<p>Your order history is currently empty</p>`
     } else if (orders.length >= 1) {
         currentOrder = orders[orders.length - 1];
-        currentOrderText = `<p>${currentOrder.desc}: ${currentOrder.price}</p>`
+        currentOrderText = `<p>- ${currentOrder.desc}: ${currentOrder.price}</p>`
     }
     
     let date1, date2;
 
     //Cutomer Message
-    date1 = Date.now();
+    date1 = new Date();
     const customerMsgContent = `
     <p class="customer-name">You</p>
-    <p class="time">${date1}</p>
+    <p class="time">${date1.getHours()}:${date1.getMinutes()}</p><br>
     <p class="customer-reply">${data.msg}</p>
     ` 
 
     //Chatbot message
-    date2 = Date.now();
+    date2 = new Date();
     const botMsgContent = 
     `
     ${botName}
-    <p class="time">${date2}</p>
+    <p class="time">${date2.getHours()}:${date2.getMinutes()}</p><br>
     <h4>Current order:</h4>
     ${currentOrderText}
-    <hr>
+    <br>
     <ul class="options">
         <li>00. Back to Main Menu</li>
     </ul>
     `
-    printMessages(customerMsgContent, botMsgContent)   
+    printMessages(customerMsgContent, botMsgContent);
+    scrollToLastMessage();
 })
 
 //Cancel order
@@ -307,26 +338,27 @@ socket.on("cancelLasOrder", (data) => {
     let date1, date2;
 
     //Cutomer Message
-    date1 = Date.now();
+    date1 = new Date();
     const customerMsgContent = `
     <p class="customer-name">You</p>
-    <p class="time">${date1}</p>
+    <p class="time">${date1.getHours()}:${date1.getMinutes()}</p><br>
     <p class="customer-reply">${data.msg}</p>
     ` 
 
     //Chatbot message
-    date2 = Date.now();
+    date2 = new Date()
     const botMsgContent = 
     `
     ${botName}
-    <p class="time">${date2}</p>
+    <p class="time">${date2.getHours()}:${date2.getMinutes()}</p><br>
     ${canceledOrderText}
-    <hr>
+    <br>
     <ul class="options">
         <li>00. Back to Main Menu</li>
     </ul>
     `
-    printMessages(customerMsgContent, botMsgContent)   
+    printMessages(customerMsgContent, botMsgContent);
+    scrollToLastMessage();
 })
 
 
